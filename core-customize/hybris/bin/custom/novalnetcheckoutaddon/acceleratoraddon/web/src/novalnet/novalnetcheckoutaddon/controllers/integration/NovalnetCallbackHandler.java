3def9c76655f1441ee1e74eb216e8f42f0791557
Novalnet AG <karthikkarthi7299@gmail.com>
2020-10-14 12:32:03 +0530

    updates

--- core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetCallbackHandler.java
+++ core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetCallbackHandler.java
@@ -81,8 +81,6 @@ public class NovalnetCallbackHandler implements BeforeControllerHandlerAdaptee {
      * @param handler  handler
      * @return Boolean
      */
-    @RequestMapping(value =
-            {"/novalnet/callback"}, method = RequestMethod.POST)
     public boolean beforeController(HttpServletRequest request, HttpServletResponse response, HandlerMethod handler) throws Exception {
 
         // Check for callback URL
