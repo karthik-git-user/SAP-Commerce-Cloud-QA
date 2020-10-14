8c34c25036e93d1d363f0ff1c4874afc6bccdc6f
Novalnet AG <karthikkarthi7299@gmail.com>
2020-10-14 19:42:52 +0530

    updates

--- core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/pages/checkout/steps/NovalnetCheckoutController.java
+++ core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/pages/checkout/steps/NovalnetCheckoutController.java
@@ -31,7 +31,6 @@ import de.hybris.platform.commercefacades.coupon.data.CouponData;
 import de.hybris.platform.commercefacades.order.CheckoutFacade;
 import de.hybris.platform.commercefacades.order.OrderFacade;
 import de.hybris.platform.commercefacades.order.data.OrderData;
-import de.hybris.platform.commercefacades.user.data.AddressData;
 import de.hybris.platform.commercefacades.order.data.OrderEntryData;
 import de.hybris.platform.commercefacades.product.ProductFacade;
 import de.hybris.platform.commercefacades.product.ProductOption;
@@ -41,7 +40,6 @@ import de.hybris.platform.commerceservices.util.ResponsiveUtils;
 import de.hybris.platform.servicelayer.exceptions.ModelNotFoundException;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
 import de.hybris.platform.yacceleratorstorefront.controllers.ControllerConstants;
-import de.hybris.platform.jalo.JaloSession;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -69,6 +67,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.WebUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static de.hybris.platform.commercefacades.constants.CommerceFacadesConstants.CONSENT_GIVEN;
 
 import novalnet.novalnetcheckoutaddon.controllers.NovalnetcheckoutaddonControllerConstants;
@@ -82,67 +82,65 @@ import novalnet.novalnetcheckoutaddon.controllers.NovalnetcheckoutaddonControlle
 public class NovalnetCheckoutController extends AbstractCheckoutController {
     protected static final Logger LOG = Logger.getLogger(NovalnetCheckoutController.class);
     /**
-     * We use this suffix pattern because of an issue with Spring 3.1 where a Uri value is incorrectly extracted if it
-     * contains on or more '.' characters. Please see https://jira.springsource.org/browse/SPR-6164 for a discussion on
-     * the issue and future resolution.
-     */
-    private static final String ORDER_CODE_PATH_VARIABLE_PATTERN = "{orderCode:.*}";
+	 * We use this suffix pattern because of an issue with Spring 3.1 where a Uri value is incorrectly extracted if it
+	 * contains on or more '.' characters. Please see https://jira.springsource.org/browse/SPR-6164 for a discussion on the
+	 * issue and future resolution.
+	 */
+	private static final String ORDER_CODE_PATH_VARIABLE_PATTERN = "{orderCode:.*}";
 
-    private static final String CONTINUE_URL_KEY = "continueUrl";
-
-    private static final String CHECKOUT_ORDER_CONFIRMATION_CMS_PAGE_LABEL = "orderConfirmation";
-
-    @Resource(name = "productFacade")
-    private ProductFacade productFacade;
+	private static final String CHECKOUT_ORDER_CONFIRMATION_CMS_PAGE_LABEL = "orderConfirmation";
+	private static final String CONTINUE_URL_KEY = "continueUrl";
+	private static final String CONSENT_FORM_GLOBAL_ERROR = "consent.form.global.error";
+	
+	@Resource(name = "productFacade")
+	private ProductFacade productFacade;
 
     @Resource(name = "orderFacade")
-    private OrderFacade orderFacade;
+	private OrderFacade orderFacade;
 
-    @Resource(name = "checkoutFacade")
-    private CheckoutFacade checkoutFacade;
+	@Resource(name = "checkoutFacade")
+	private CheckoutFacade checkoutFacade;
 
-    @Resource(name = "guestRegisterValidator")
-    private GuestRegisterValidator guestRegisterValidator;
+	@Resource(name = "guestRegisterValidator")
+	private GuestRegisterValidator guestRegisterValidator;
 
-    @Resource(name = "autoLoginStrategy")
-    private AutoLoginStrategy autoLoginStrategy;
+	@Resource(name = "autoLoginStrategy")
+	private AutoLoginStrategy autoLoginStrategy;
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * de.hybris.platform.storefront.controllers.pages.CheckoutController#handleModelNotFoundException
-     * (javax.servlet.http.HttpServletRequest)
-     */
-    @ExceptionHandler(ModelNotFoundException.class)
-    public String handleModelNotFoundException(final ModelNotFoundException exception, final HttpServletRequest request) {
-        request.setAttribute("message", exception.getMessage());
-        return FORWARD_PREFIX + "/404";
-    }
+	@Resource(name = "consentFacade")
+	protected ConsentFacade consentFacade;
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * de.hybris.platform.storefront.controllers.pages.CheckoutController#checkout
-     * (javax.servlet.http.HttpServletRequest)
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String checkout(final RedirectAttributes redirectModel) {
-        if (getCheckoutFlowFacade().hasValidCart()) {
-            if (validateCart(redirectModel)) {
-                return REDIRECT_PREFIX + "/cart";
-            } else {
-                checkoutFacade.prepareCartForCheckout();
-                return getCheckoutRedirectUrl();
-            }
-        }
+	@Resource(name = "customerConsentDataStrategy")
+	protected CustomerConsentDataStrategy customerConsentDataStrategy;
 
-        LOG.info("Missing, empty or unsupported cart");
+	@ExceptionHandler(ModelNotFoundException.class)
+	public String handleModelNotFoundException(final ModelNotFoundException exception, final HttpServletRequest request)
+	{
+		request.setAttribute("message", exception.getMessage());
+		return FORWARD_PREFIX + "/404";
+	}
 
-        // No session cart or empty session cart. Bounce back to the cart page.
-        return REDIRECT_PREFIX + "/cart";
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String checkout(final RedirectAttributes redirectModel)
+	{
+		if (getCheckoutFlowFacade().hasValidCart())
+		{
+			if (validateCart(redirectModel))
+			{
+				return REDIRECT_PREFIX + "/cart";
+			}
+			else
+			{
+				checkoutFacade.prepareCartForCheckout();
+				return getCheckoutRedirectUrl();
+			}
+		}
+
+		LOG.info("Missing, empty or unsupported cart");
+
+		// No session cart or empty session cart. Bounce back to the cart page.
+		return REDIRECT_PREFIX + "/cart";
+	}
 
     /*
      * (non-Javadoc)
@@ -154,10 +152,11 @@ public class NovalnetCheckoutController extends AbstractCheckoutController {
     @RequestMapping(value = "novalnet/orderConfirmation/" + ORDER_CODE_PATH_VARIABLE_PATTERN, method = RequestMethod.GET)
     @RequireHardLogIn
     public String orderConfirmation(@PathVariable("orderCode") final String orderCode, final HttpServletRequest request,
-                                    final Model model) throws CMSItemNotFoundException {
-        SessionOverrideCheckoutFlowFacade.resetSessionOverrides();
-        return processOrderCode(orderCode, model, request);
-    }
+			final Model model, final RedirectAttributes redirectModel) throws CMSItemNotFoundException
+	{
+		SessionOverrideCheckoutFlowFacade.resetSessionOverrides();
+		return processOrderCode(orderCode, model, request, redirectModel);
+	}
 
     /*
      * (non-Javadoc)
@@ -168,25 +167,12 @@ public class NovalnetCheckoutController extends AbstractCheckoutController {
      */
     @RequestMapping(value = "novalnet/orderConfirmation/" + ORDER_CODE_PATH_VARIABLE_PATTERN, method = RequestMethod.POST)
     public String orderConfirmation(final GuestRegisterForm form, final BindingResult bindingResult, final Model model,
-                                    final HttpServletRequest request, final HttpServletResponse response, final RedirectAttributes redirectModel)
-            throws CMSItemNotFoundException {
-        getGuestRegisterValidator().validate(form, bindingResult);
-        return processRegisterGuestUserRequest(form, bindingResult, model, request, response, redirectModel);
-    }
-
-    /**
-     * Method used to determine the checkout redirect URL that will handle the checkout process.
-     *
-     * @return A <code>String</code> object of the URL to redirect to.
-     */
-    protected String getCheckoutRedirectUrl() {
-        if (getUserFacade().isAnonymousUser()) {
-            return REDIRECT_PREFIX + "/login/checkout";
-        }
-
-        // Default to the multi-step checkout
-        return REDIRECT_PREFIX + "/checkout/multi";
-    }
+			final HttpServletRequest request, final HttpServletResponse response, final RedirectAttributes redirectModel)
+			throws CMSItemNotFoundException
+	{
+		getGuestRegisterValidator().validate(form, bindingResult);
+		return processRegisterGuestUserRequest(form, bindingResult, model, request, response, redirectModel);
+	}
 
     /*
      * (non-Javadoc)
@@ -270,104 +256,96 @@ public class NovalnetCheckoutController extends AbstractCheckoutController {
 		return REDIRECT_PREFIX + "/";
 	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * de.hybris.platform.storefront.controllers.pages.CheckoutController#processOrderCode
-     * (javax.servlet.http.HttpServletRequest)
-     */
-    protected String processOrderCode(final String orderCode, final Model model, final HttpServletRequest request)
-            throws CMSItemNotFoundException {
-        final OrderData orderDetails;
+	protected String processOrderCode(final String orderCode, final Model model, final HttpServletRequest request,
+			final RedirectAttributes redirectModel) throws CMSItemNotFoundException
+	{
+		final OrderData orderDetails;
 
-        try {
-            orderDetails = orderFacade.getOrderDetailsForCode(orderCode);
-        } catch (final UnknownIdentifierException e) {
-            LOG.warn("Attempted to load an order confirmation that does not exist or is not visible. Redirect to home page.");
-            return REDIRECT_PREFIX + ROOT;
-        }
+		try
+		{
+			orderDetails = orderFacade.getOrderDetailsForCode(orderCode);
+		}
+		catch (final UnknownIdentifierException e)
+		{
+			LOG.warn("Attempted to load an order confirmation that does not exist or is not visible. Redirect to home page.");
+			return REDIRECT_PREFIX + ROOT;
+		}
 
-        addRegistrationConsentDataToModel(model);
+		addRegistrationConsentDataToModel(model);
 
-        if (orderDetails.isGuestCustomer() && !StringUtils.substringBefore(orderDetails.getUser().getUid(), "|")
-                .equals(getSessionService().getAttribute(WebConstants.ANONYMOUS_CHECKOUT_GUID))) {
-            return getCheckoutRedirectUrl();
-        }
+		if (orderDetails.isGuestCustomer() && !StringUtils.substringBefore(orderDetails.getUser().getUid(), "|")
+				.equals(getSessionService().getAttribute(WebConstants.ANONYMOUS_CHECKOUT_GUID)))
+		{
+			return getCheckoutRedirectUrl();
+		}
 
-        if (orderDetails.getEntries() != null && !orderDetails.getEntries().isEmpty()) {
-            for (final OrderEntryData entry : orderDetails.getEntries()) {
-                final String productCode = entry.getProduct().getCode();
-                final ProductData product = productFacade.getProductForCodeAndOptions(productCode,
-                        Arrays.asList(ProductOption.BASIC, ProductOption.PRICE, ProductOption.CATEGORIES));
-                entry.setProduct(product);
-            }
-        }
+		if (orderDetails.getEntries() != null && !orderDetails.getEntries().isEmpty())
+		{
+			for (final OrderEntryData entry : orderDetails.getEntries())
+			{
+				final String productCode = entry.getProduct().getCode();
+				final ProductData product = productFacade.getProductForCodeAndOptions(productCode,
+						Arrays.asList(ProductOption.BASIC, ProductOption.PRICE, ProductOption.CATEGORIES));
+				entry.setProduct(product);
+			}
+		}
 
-        model.addAttribute("orderCode", orderCode);
-        model.addAttribute("orderData", orderDetails);
-        model.addAttribute("allItems", orderDetails.getEntries());
-        model.addAttribute("deliveryAddress", orderDetails.getDeliveryAddress());
-        model.addAttribute("deliveryMode", orderDetails.getDeliveryMode());
-        model.addAttribute("paymentInfo", orderDetails.getPaymentInfo());
-        model.addAttribute("pageType", PageType.ORDERCONFIRMATION.name());
+       model.addAttribute("orderCode", orderCode);
+		model.addAttribute("orderData", orderDetails);
+		model.addAttribute("allItems", orderDetails.getEntries());
+		model.addAttribute("deliveryAddress", orderDetails.getDeliveryAddress());
+		model.addAttribute("deliveryMode", orderDetails.getDeliveryMode());
+		model.addAttribute("paymentInfo", orderDetails.getPaymentInfo());
+		model.addAttribute("pageType", PageType.ORDERCONFIRMATION.name());
         model.addAttribute("tid", getSessionService().getAttribute("tid"));
         AddressData addressData = getSessionService().getAttribute("novalnetAddressData");
 		model.addAttribute("addressDetails", addressData);
 
         final List<CouponData> giftCoupons = orderDetails.getAppliedOrderPromotions().stream()
-                .filter(x -> CollectionUtils.isNotEmpty(x.getGiveAwayCouponCodes())).flatMap(p -> p.getGiveAwayCouponCodes().stream())
-                .collect(Collectors.toList());
-        model.addAttribute("giftCoupons", giftCoupons);
+				.filter(x -> CollectionUtils.isNotEmpty(x.getGiveAwayCouponCodes())).flatMap(p -> p.getGiveAwayCouponCodes().stream())
+				.collect(Collectors.toList());
+		model.addAttribute("giftCoupons", giftCoupons);
 
-        processEmailAddress(model, orderDetails);
+		processEmailAddress(model, orderDetails);
 
-        final String continueUrl = (String) getSessionService().getAttribute(WebConstants.CONTINUE_URL);
-        model.addAttribute(CONTINUE_URL_KEY, (continueUrl != null && !continueUrl.isEmpty()) ? continueUrl : ROOT);
+		final String continueUrl = (String) getSessionService().getAttribute(WebConstants.CONTINUE_URL);
+		model.addAttribute(CONTINUE_URL_KEY, (continueUrl != null && !continueUrl.isEmpty()) ? continueUrl : ROOT);
 
-        final ContentPageModel checkoutOrderConfirmationPage = getContentPageForLabelOrId(CHECKOUT_ORDER_CONFIRMATION_CMS_PAGE_LABEL);
-        storeCmsPageInModel(model, checkoutOrderConfirmationPage);
-        setUpMetaDataForContentPage(model, checkoutOrderConfirmationPage);
-        model.addAttribute(ThirdPartyConstants.SeoRobots.META_ROBOTS, ThirdPartyConstants.SeoRobots.NOINDEX_NOFOLLOW);
+		final ContentPageModel checkoutOrderConfirmationPage = getContentPageForLabelOrId(CHECKOUT_ORDER_CONFIRMATION_CMS_PAGE_LABEL);
+		storeCmsPageInModel(model, checkoutOrderConfirmationPage);
+		setUpMetaDataForContentPage(model, checkoutOrderConfirmationPage);
+		model.addAttribute(ThirdPartyConstants.SeoRobots.META_ROBOTS, ThirdPartyConstants.SeoRobots.NOINDEX_NOFOLLOW);
 
         return NovalnetcheckoutaddonControllerConstants.Views.Pages.Checkout.CheckoutPage;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * de.hybris.platform.storefront.controllers.pages.CheckoutController#getGuestRegisterValidator
-     * (javax.servlet.http.HttpServletRequest)
-     */
-    protected GuestRegisterValidator getGuestRegisterValidator() {
-        return guestRegisterValidator;
-    }
+    protected void processEmailAddress(final Model model, final OrderData orderDetails)
+	{
+		final String uid;
 
-    protected void processEmailAddress(final Model model, final OrderData orderDetails) {
-        final String uid;
-
-        if (orderDetails.isGuestCustomer() && !model.containsAttribute("guestRegisterForm")) {
-            final GuestRegisterForm guestRegisterForm = new GuestRegisterForm();
-            guestRegisterForm.setOrderCode(orderDetails.getGuid());
+		if (orderDetails.isGuestCustomer() && !model.containsAttribute("guestRegisterForm"))
+		{
+			final GuestRegisterForm guestRegisterForm = new GuestRegisterForm();
+			guestRegisterForm.setOrderCode(orderDetails.getGuid());
             uid = getSessionService().getAttribute("email");
             guestRegisterForm.setUid(uid);
-            model.addAttribute(guestRegisterForm);
-        } else {
-            uid = orderDetails.getUser().getUid();
-        }
-        model.addAttribute("email", uid);
-    }
+			model.addAttribute(guestRegisterForm);
+		}
+		else
+		{
+			uid = orderDetails.getUser().getUid();
+		}
+		model.addAttribute("email", uid);
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * de.hybris.platform.storefront.controllers.pages.CheckoutController#getAutoLoginStrategy
-     * (javax.servlet.http.HttpServletRequest)
-     */
-    protected AutoLoginStrategy getAutoLoginStrategy() {
-        return autoLoginStrategy;
-    }
+    protected GuestRegisterValidator getGuestRegisterValidator()
+	{
+		return guestRegisterValidator;
+	}
+
+	protected AutoLoginStrategy getAutoLoginStrategy()
+	{
+		return autoLoginStrategy;
+	}
 
 }
