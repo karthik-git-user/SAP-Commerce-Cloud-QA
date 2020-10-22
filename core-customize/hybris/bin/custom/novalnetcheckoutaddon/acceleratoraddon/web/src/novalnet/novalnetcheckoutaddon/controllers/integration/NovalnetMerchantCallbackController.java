26e7d156b4e3bd4ba5375eb90dca007423209c3d
Novalnet AG <karthikkarthi7299@gmail.com>
2020-10-22 19:02:19 +0530

    updates

--- core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetMerchantCallbackController.java
+++ core-customize/hybris/bin/custom/novalnetcheckoutaddon/acceleratoraddon/web/src/novalnet/novalnetcheckoutaddon/controllers/integration/NovalnetMerchantCallbackController.java
@@ -599,10 +599,12 @@ public class NovalnetMerchantCallbackController extends BaseIntegrationControlle
 		
 		//~ final HttpHeaders headers = DefaultCsrfOutboundRequestDecorator.getHeadersForTokenFetching();
 		public static final String TOKEN_EMPTY = Config.getParameter("customerticketingc4cintegration.c4c-token-empty");
+		public static final String TOKEN_EMPTY1 = Config.getParameter("fetch");
 		
 		System.out.println(map);
 		System.out.println("============================== print ==================================");
 		System.out.println(TOKEN_EMPTY);
+		System.out.println(TOKEN_EMPTY1);
 		System.out.println("============================== print ==================================");
 		
 			
